45b621361852404a41a6dc3d494609f83978e04a
beatlz <adrian@basave.com>
2022-06-20 00:08:01 +0200

    Added index.ts sample code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 require(`module-alias/register`)
+
+import getChord from "@modules/getChord"
+import sharpToFlat from "@modules/sharpToFlat"
+import buildScale from "@modules/buildScale"
+// import getScale from "@modules/getScale"
+import scaleTemplates from "@resources/scaleTemplates"
+
+const chord = getChord(`C`, `sus4`)
+const flat = sharpToFlat(`A#`) 
+const scale = buildScale(`A`, scaleTemplates[`harmonicMinor`])
+
+console.log(chord)
+console.log(flat)
+console.log(scale)
